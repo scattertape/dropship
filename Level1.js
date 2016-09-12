e430a097103e32bbe3beab064f02767987a7f4fa
scattertape <scattertape@gmail.com>
2016-09-12 22:28:32 +0100

    Add files via upload

--- Level1.js
+++ Level1.js
@@ -782,10 +782,10 @@ var Dropship;
                             this._text1.setText("y:" + deviceMo.acceleration.y.toFixed(3) + ", x:" + deviceMo.acceleration.x.toFixed(3) + ", z:" + deviceMo.acceleration.z.toFixed(3));
                             this._text2.setText("y:" + deviceMo.accelerationIncludingGravity.y.toFixed(3) + ", x:" + deviceMo.accelerationIncludingGravity.x.toFixed(3) + ", z:" + deviceMo.accelerationIncludingGravity.z.toFixed(3));
                             if (this.landscapeLayout == true) {
-                                this._base.body.rotation = this._base.body.rotation + deviceMo.accelerationIncludingGravity.y;
+                                this._base.body.rotation = this._base.body.rotation + (deviceMo.accelerationIncludingGravity.y * 0.01);
                             }
                             else {
-                                this._base.body.rotation = this._base.body.rotation + deviceMo.accelerationIncludingGravity.x;
+                                this._base.body.rotation = this._base.body.rotation + (deviceMo.accelerationIncludingGravity.x * 0.01);
                             }
                         }
                     }
