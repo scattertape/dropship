8f689a4a99e1a1e4f994f86ad276c6574b96afb8
scattertape <scattertape@gmail.com>
2016-09-14 19:24:34 +0100

    Add files via upload

--- Level1.js
+++ Level1.js
@@ -789,11 +789,11 @@ var Dropship;
                                 //this._base.body.angle = this._base.body.angle + deviceMo.accelerationIncludingGravity.y 
                                 if (currentMotion > this.prevMotion) {
                                     // turn ship clockwise
-                                    this._base.body.angle = this._base.body.angle + (difference(currentMotion, this.prevMotion) * 10);
+                                    this._base.body.angle = this._base.body.angle + (difference(currentMotion, this.prevMotion) * 25);
                                 }
                                 else {
                                     // turn ship anticlockwise
-                                    this._base.body.angle = this._base.body.angle - (difference(currentMotion, this.prevMotion) * 10);
+                                    this._base.body.angle = this._base.body.angle - (difference(currentMotion, this.prevMotion) * 25);
                                 }
                                 /*  if (this.prevMotion > minMotion) {
                                       newDiff = this.prevMotion - minMotion;
