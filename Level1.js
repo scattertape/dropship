8ad71f1f51db3f67f482ef9143c8b437c3847650
scattertape <scattertape@gmail.com>
2016-09-16 07:37:14 +0100

    Add files via upload

--- Level1.js
+++ Level1.js
@@ -801,7 +801,7 @@ var Dropship;
                             //this._base.body.rotateLeft(deviceMo.accelerationIncludingGravity.y);
                             //newAngle = difference(currentMotion, this.prevMotion);
                             //this._text2.setText('newAngle: ' + newAngle);
-                            this._base.body.angularVelocity += (currentMotion);
+                            this._base.body.angularVelocity += (currentMotion * 0.01);
                             this._text1.setText(this._base.body.angularVelocity);
                             //(<Phaser.Physics.P2.Body>this._base.body).angularVelocity = currentMotion;
                             /* if (currentMotion > this.prevMotion) {
