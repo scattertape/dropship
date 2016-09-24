0ed0c44629207f6bdaad40a08c46b0162fbc2387
scattertape <scattertape@gmail.com>
2016-09-24 21:26:26 +0100

    Add files via upload

--- Level1.js
+++ Level1.js
@@ -783,7 +783,7 @@ var Dropship;
                                 currentMotion = deviceMo.accelerationIncludingGravity.y;
                             }
                             else {
-                                currentMotion = deviceMo.accelerationIncludingGravity.z; //x
+                                currentMotion = deviceMo.accelerationIncludingGravity.x;
                             }
                             var invertRotation = true;
                             if (invertRotation) {
