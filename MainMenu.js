4e107d6878937929a19250e80bdef047c00b4d5d
scattertape <scattertape@gmail.com>
2016-09-24 23:50:32 +0100

    Add files via upload

--- MainMenu.js
+++ MainMenu.js
@@ -5,14 +5,24 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var Dropship;
 (function (Dropship) {
+    var moArray = [];
+    window.addEventListener("devicemotion", onDeviceMotionTest, false);
+    function onDeviceMotionTest(event) {
+        console.log('onDeviceMotionTest');
+        alert('dmo');
+        if (event.acceleration != null) {
+            if (event.accelerationIncludingGravity.y != null) {
+                moArray.push(event.accelerationIncludingGravity.y);
+            }
+        }
+    }
     var MainMenu = (function (_super) {
         __extends(MainMenu, _super);
         function MainMenu() {
             _super.apply(this, arguments);
-            this.deviceMoArray = [];
+            this.deviceMoArray = 0;
         }
         MainMenu.prototype.create = function () {
-            window.addEventListener("devicemotion", this.onDeviceMotionTest, false);
             this.background = this.add.sprite(this.game.width * 0.5, this.game.height * 0.5, 'titlepage');
             this.background.alpha = 0;
             this.background.anchor.setTo(0.5, 0.5);
@@ -22,22 +32,14 @@ var Dropship;
             this.add.tween(this.logo).to({ y: 220 }, 200, Phaser.Easing.Elastic.Out, true, 200);
             this.input.onDown.addOnce(this.fadeOut, this);
         };
-        MainMenu.prototype.onDeviceMotionTest = function (event) {
-            console.log('onDeviceMotionTest');
-            alert('mo');
-            if (event.acceleration != null) {
-                if (event.accelerationIncludingGravity.y != null) {
-                    this.deviceMoArray.push(event.accelerationIncludingGravity.y);
-                }
-            }
-        };
         MainMenu.prototype.fadeOut = function () {
             this.add.tween(this.background).to({ alpha: 0 }, 150, Phaser.Easing.Linear.None, true);
             var tween = this.add.tween(this.logo).to({ y: 800 }, 150, Phaser.Easing.Linear.None, true);
             tween.onComplete.add(this.startGame, this);
         };
         MainMenu.prototype.startGame = function () {
-            window.removeEventListener("devicemotion", this.onDeviceMotionTest);
+            window.removeEventListener("devicemotion", onDeviceMotionTest);
+            this.deviceMoArray = moArray.length;
             this.game.state.start('Level1', true, false);
         };
         return MainMenu;
