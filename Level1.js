06e08d4b410702476b6e19b6382c626685a4e2bd
scattertape <scattertape@gmail.com>
2016-10-02 23:29:13 +0100

    Add files via upload

--- Level1.js
+++ Level1.js
@@ -823,11 +823,11 @@ var Dropship;
                     }
                     if (currentMotion > 0) {
                         // newAngle = map_range(currentMotion, 0, 5, 0, 180);
-                        newAngle = logslider(currentMotion, 0, 5, 0, 180);
+                        newAngle = logslider(currentMotion, 0, 5, 2.5, 180);
                     }
                     else {
                         // newAngle = map_range(currentMotion, -5, 0, -180, 0);
-                        newAngle = 0 - logslider(Math.abs(currentMotion), 0, 5, 0, 180);
+                        newAngle = 0 - logslider(Math.abs(currentMotion), 0, 5, 2.5, 180);
                     }
                     var movementDifference = difference(newAngle, this.motionTracker[0]);
                     if (Math.abs(movementDifference) > 8) {
